17c98ffc56d4a9de1a0a7c1b2644dd7ef1b3f620
brobdingrag <brobdingrag@gmail.com>
2025-12-25 21:45:51 -0500

    add new cluster

--- cluster.sh
+++ cluster.sh
@@ -1,49 +1,29 @@
 #!/bin/bash
 
-# =========================
-# Set up working directory
-# =========================
-
 set -euo pipefail
 
-mkdir -p data  # Create data directory if it does not exist
-cd data        # Navigate into data directory
+# Set up working directory
+echo "Setting up 'data/' directory..."
+mkdir -p data
+cd data
 
-# =========================
 # Download input data files
-# =========================
-
-# Genotype data in .pgen (compressed) format
+echo "Downloading input genotype and sample metadata files..."
 wget https://www.dropbox.com/s/j72j6uciq5zuzii/all_hg38.pgen.zst
-
-# Variant information in .pvar (compressed) format
 wget https://www.dropbox.com/scl/fi/fn0bcm5oseyuawxfvkcpb/all_hg38_rs.pvar.zst?rlkey=przncwb78rhz4g4ukovocdxaz
-
-# Sample information file (.psam)
 wget https://www.dropbox.com/scl/fi/u5udzzaibgyvxzfnjcvjc/hg38_corrected.psam?rlkey=oecjnk4vmbhc8b1p202l0ih4x
-
-# List of sample IDs to remove (e.g., relateds from KING)
 wget https://www.dropbox.com/s/4zhmxpk5oclfplp/deg2_hg38.king.cutoff.out.id
-
-# 1000 Genomes project sample metadata (population info)
 wget -O samples.txt https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/20130606_g1k_3202_samples_ped_population.txt
 
-# =====================================
 # Clean up and decompress downloaded files
-# =====================================
-
-# Rename .pvar and .psam files to remove query string from Dropbox shared links
+echo "Renaming and decompressing downloaded files..."
 mv all_hg38_rs.pvar.zst?rlkey=przncwb78rhz4g4ukovocdxaz all_hg38.pvar.zst
 mv hg38_corrected.psam?rlkey=oecjnk4vmbhc8b1p202l0ih4x all_hg38.psam
-
-# Decompress pvar and pgen files using plink2
 plink2 --zst-decompress all_hg38.pvar.zst > all_hg38.pvar
 plink2 --zst-decompress all_hg38.pgen.zst > all_hg38.pgen
 
-# ========================================
 # Remove related individuals from genotype data
-# ========================================
-
+echo "Removing related individuals from genotype dataset and filtering SNPs..."
 plink2 --pfile all_hg38 vzs \
   --remove deg2_hg38.king.cutoff.out.id \
   --chr 1-22 \
@@ -56,12 +36,8 @@ plink2 --pfile all_hg38 vzs \
   --memory 20000 \
   --threads 6
 
-
-# ========================================
-# Create new PLINK binary fileset using:
-#   --maf 0.01   => remove rare variants (MAF < 1%)
-# ========================================
-
+# Filter by minor allele frequency (MAF)
+echo "Filtering variants by MAF >= 0.01..."
 plink2 --bfile global_before_qc \
   --maf 0.01 \
   --make-bed \
@@ -69,21 +45,16 @@ plink2 --bfile global_before_qc \
   --memory 12000 \
   --threads 6
 
-# ========================================
-# 2) LD prune (ADMIXTURE manual-style)
-#    Window = 50 SNPs, step = 10 SNPs, r^2 threshold = 0.1
-# ========================================
-
+# LD pruning
+echo "LD pruning SNPs (window 50, step 10, r2 0.1)..."
 plink2 --bfile global_qc_maf \
   --indep-pairwise 50 10 0.1 \
   --out global_ldprune \
   --memory 12000 \
   --threads 6
 
-# ========================================
 # Make the pruned dataset for ADMIXTURE
-# ========================================
-
+echo "Constructing LD-pruned dataset for ADMIXTURE..."
 plink2 --bfile global_qc_maf \
   --extract global_ldprune.prune.in \
   --make-bed \
@@ -91,11 +62,8 @@ plink2 --bfile global_qc_maf \
   --memory 12000 \
   --threads 6
 
-# ========================================
 # Clean up intermediate and temporary files
-# ========================================
-
-# Remove raw downloads + decompressed inputs
+echo "Cleaning up intermediate and temporary files..."
 rm -f \
   all_hg38.pgen.zst \
   all_hg38.pvar.zst \
@@ -104,23 +72,18 @@ rm -f \
   all_hg38.psam \
   deg2_hg38.king.cutoff.out.id
 
-# Remove intermediate PLINK outputs
 rm -f \
   global_before_qc* \
   global_qc_maf* \
   global_ldprune*
 
-
-# ========================================
-# Run ADMIXTURE with 5 clusters and 6 threads
-# ========================================
-
+# Run ADMIXTURE
+echo "Running ADMIXTURE (K=5, threads=6, seed=12345)..."
 admixture -j6 --seed=12345 global.bed 5
 
-
-# Remove ADMIXTURE inputs 
+# Remove ADMIXTURE input and log files
+echo "Removing ADMIXTURE input and log files..."
 rm -f \
   global.bed \
   global.log 
-
 
